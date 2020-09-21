ccb488ef2eb70b952ee5969062b9ac728442e550
kaede0902 <kaede0920js@gmail.com>
2020-09-21 19:19:53 +0900

    renamed classNames to MainMeat, WhiteEye, BlueEye,

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import './styles/body.css'
 export default function App() {
   document.title = `expo2025`;
   return (
-    <div className="wrapper">
+    <div className="container">
       wrapper area
-      <div className="head head-one"></div>
-      <div className="head head-two"></div>
-      <div className="head head-three"></div>
-      <div className="whiteEye eyeOne"></div>
+      <div className="core"></div>
+
+      <div className="MainMeat MainMeatOne"></div>
+      <div className="WhiteEye WhiteEyeOne"></div>
       <div className="blueEye blueEyeOne"></div>
     </div>
   );
