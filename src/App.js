95593ca2eb73b65b8adcd36849371ed2a3726bae
kaede0902 <kaede0920js@gmail.com>
2020-09-21 23:04:50 +0900

    mod App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import './styles/global.css'
-import './styles/body.css'
+
+import './styles/MainMeat.css'
+import './styles/WhiteEye.css'
+import './styles/BlueEye.css'
 
 export default function App() {
   document.title = `expo2025`;
   return (
     <div className="container">
-      wrapper area
-      <div className="core"></div>
-
+      LIFE IS SHINING...
       <div className="MainMeat MainMeatOne"></div>
+      <div className="MainMeat MainMeatTwo"></div>
+      <div className="MainMeat MainMeatThree"></div>
+      <div className="MainMeat MainMeatFour"></div>
+      <div className="MainMeat MainMeatFive"></div>
+
       <div className="WhiteEye WhiteEyeOne"></div>
-      <div className="blueEye blueEyeOne"></div>
+      <div className="WhiteEye WhiteEyeTwo"></div>
+      <div className="WhiteEye WhiteEyeThree"></div>
+      <div className="WhiteEye WhiteEyeFour"></div>
+      <div className="WhiteEye WhiteEyeFive"></div>
+
+
+      <div className="BlueEye BlueEyeOne"></div>
+      <div className="BlueEye BlueEyeTwo"></div>
+      <div className="BlueEye BlueEyeThree"></div>
+      <div className="BlueEye BlueEyeFour"></div>
+      <div className="BlueEye BlueEyeFive"></div>
     </div>
   );
 }
